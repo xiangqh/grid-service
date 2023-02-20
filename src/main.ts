8160d1169dc03e32e242d9c2caf495277c360cf1
xiangqh <working_cat@126.com>
2023-02-21 00:55:41 +0800

    set port

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import * as session from 'express-session';
 import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({credentials:true, origin:true});
   app.use(cookieParser());
-  await app.listen(3000);
+
+  const configService = app.get(ConfigService);
+  await app.listen(configService.get("port", "3000"));
+  console.log(`app start port ${configService.get("port", "3000")}`, );
 }
 bootstrap();
